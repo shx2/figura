15ddf82f87323196b378a2bc3621a3bc017bcde9
shx2 <shx222@gmail.com>
2016-12-01 14:43:15 +0200

    Default figura file extension is back to ".py"
    Will switch to ".fig" in V2.
    Added figura.settings.set_extension_fig() for fwd-compatibility.

--- figura/settings.py
+++ figura/settings.py
@@ -8,7 +8,8 @@ from .misc import Struct
 ################################################################################
 # Defaults
 
-DEFAULT_CONFIG_FILE_EXT = 'fig'
+DEFAULT_CONFIG_FILE_EXT = 'py'  # This is currently the default, in V1
+#DEFAULT_CONFIG_FILE_EXT = 'fig'  # This will be enabled in V2.0 -- see set_extension_fig()
 
 ################################################################################
 # The SETTINGS
@@ -26,5 +27,19 @@ def set_setting(key, new_value):
 
 def get_setting(key, *default):
     return getattr(SETTINGS, key, *default)
+
+################################################################################
+
+def set_extension_fig():
+    """
+    Use this for forward-compatibility.  This enables the ".fig" extension
+    (which will be the default in V2) while using Figura V1.
+    
+    Use like::
+    
+        from figura.settings import set_extension_fig; set_extension_fig()
+     
+    """
+    return set_setting('CONFIG_FILE_EXT', 'fig')
 
 ################################################################################
---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+
+# Tests use ".fig" extension. Enable it:
+from figura.settings import set_extension_fig as _setfig
+_setfig()
